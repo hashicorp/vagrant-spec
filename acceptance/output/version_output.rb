67dd1e255ca683d0fa71f0090953503328f78ec4
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-04 23:18:54 -0800

    vagrant/core: version output matcher works

--- acceptance/output/version_output.rb
+++ acceptance/output/version_output.rb
@@ -4,7 +4,7 @@ module Vagrant
   module Spec
     # Tests the Vagrant version output
     OutputTester[:version] = lambda do |text|
-      text =~ /^Vagrant version (\d+\.\d+\.\d+(\.[a-z0-9]+)?)$/
+      text =~ /^Vagrant (\d+\.\d+\.\d+(\.[a-z0-9]+)?)$/
     end
   end
 end
