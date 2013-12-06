ec1291a5d8ae9b740f72149e3eb0424d9b00793b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-06 13:06:53 -0800

    acceptance/cli: test exit code for version

--- acceptance/cli/version_spec.rb
+++ acceptance/cli/version_spec.rb
@@ -6,11 +6,13 @@ describe "vagrant CLI basics", component: "cli" do
   describe "vagrant version" do
     it "prints the version when called with '-v'" do
       result = execute("vagrant", "-v")
+      expect(result).to exit_with(0)
       expect(result.stdout).to match_output(:version)
     end
 
     it "prints the version when called with '--version'" do
       result = execute("vagrant", "--version")
+      expect(result).to exit_with(0)
       expect(result.stdout).to match_output(:version)
     end
   end
