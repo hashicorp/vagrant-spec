589e686b293ee984bd18f882a1cea614a4d31a27
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-05 17:00:02 -0800

    get the synced folder tests running properly

--- lib/vagrant-spec/acceptance/rspec/shared_provider_synced_folder.rb
+++ lib/vagrant-spec/acceptance/rspec/shared_provider_synced_folder.rb
@@ -10,25 +10,23 @@ shared_examples "provider/synced_folder" do |provider, options|
   # Spin up a single VM to test all the synced folder configuration
   # for speed. Besides, its a good test to make sure they all work
   # together.
-  before(:all) do |example|
+  before do
     environment.skeleton("synced_folders")
-
     assert_execute("vagrant", "box", "add", "basic", options[:box_basic])
-    assert_execute("vagrant", "init", "basic")
     assert_execute("vagrant", "up", "--provider=#{provider}")
   end
 
-  after(:all) do |example|
+  after do
     assert_execute("vagrant", "destroy", "--force")
   end
 
-  it "mounts the default /vagrant synced folder" do
+  it "properly configures synced folder types" do
+    status("Test: mounts the default /vagrant synced folder")
     result = execute("vagrant", "ssh", "-c", "cat /vagrant/foo")
     expect(result.exit_code).to eql(0)
     expect(result.stdout).to match(/hello\n$/)
-  end
 
-  it "doesn't mount a disabled folder" do
+    status("Test: doesn't mount a disabled folder")
     result = execute("vagrant", "ssh", "-c", "test -d /foo")
     expect(result.exit_code).to eql(1)
   end
