9894c92f73164c8d782410268712a28086958bf9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-06 14:39:13 -0800

    acceptance/cli/init

---
+++ acceptance/cli/init_spec.rb
@@ -0,0 +1,42 @@
+describe "vagrant CLI: init", component: "cli/init" do
+  include_context "acceptance"
+
+  it "creates a Vagrantfile in the working directory" do
+    vagrantfile = environment.workdir.join("Vagrantfile")
+    expect(vagrantfile.exist?).to_not be_true
+
+    assert_execute("vagrant", "init")
+    expect(vagrantfile.exist?).to be_true
+  end
+
+  it "creates a Vagrantfile with the box set to the given argument" do
+    vagrantfile = environment.workdir.join("Vagrantfile")
+
+    assert_execute("vagrant", "init", "foo")
+    vagrantfile.read.should match(/config.vm.box = "foo"$/)
+  end
+
+  it "creates a Vagrantfile with the box URL set to the given argument" do
+    vagrantfile = environment.workdir.join("Vagrantfile")
+
+    assert_execute("vagrant", "init", "foo", "bar")
+
+    contents = vagrantfile.read
+    expect(contents).to match(/config.vm.box = "foo"$/)
+    expect(contents).to match(/config.vm.box_url = "bar"$/)
+  end
+
+  it "outputs the Vagrantfile to another location if specified" do
+    vagrantfile = environment.workdir.join("foo")
+
+    assert_execute("vagrant", "init", "--output", "foo")
+    expect(vagrantfile.exist?).to be_true
+    expect(vagrantfile.read).to match(/^Vagrant\.configure/)
+  end
+
+  it "outputs the Vagrantfile to stdout if specified" do
+    result = execute("vagrant", "init", "--output", "-")
+    expect(result).to exit_with(0)
+    expect(result.stdout).to match(/^Vagrant\.configure/)
+  end
+end
--- acceptance/cli/version_spec.rb
+++ acceptance/cli/version_spec.rb
@@ -1,19 +1,15 @@
-require "vagrant-spec/acceptance"
-
-describe "vagrant CLI basics", component: "cli" do
+describe "vagrant CLI: version", component: "cli/version" do
   include_context "acceptance"
 
-  describe "vagrant version" do
-    it "prints the version when called with '-v'" do
-      result = execute("vagrant", "-v")
-      expect(result).to exit_with(0)
-      expect(result.stdout).to match_output(:version)
-    end
+  it "prints the version when called with '-v'" do
+    result = execute("vagrant", "-v")
+    expect(result).to exit_with(0)
+    expect(result.stdout).to match_output(:version)
+  end
 
-    it "prints the version when called with '--version'" do
-      result = execute("vagrant", "--version")
-      expect(result).to exit_with(0)
-      expect(result.stdout).to match_output(:version)
-    end
+  it "prints the version when called with '--version'" do
+    result = execute("vagrant", "--version")
+    expect(result).to exit_with(0)
+    expect(result.stdout).to match_output(:version)
   end
 end
