e6d3b96add86710ddb86f907899c513bc7cad82e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-01-07 09:34:19 -0800

    plugin tests test installing specific versions, remote, updating

--- acceptance/cli/plugin_spec.rb
+++ acceptance/cli/plugin_spec.rb
@@ -9,6 +9,30 @@ describe "vagrant CLI: plugin", component: "cli/plugin" do
     expect(result.stdout).to match_output(:plugin_list_none)
   end
 
+  it "has certain behavior with a plugin installed" do
+    status("Test: plugin installation")
+    path = Vagrant::Spec.acceptance_plugin_path.join("vagrant-spec-helper-basic-0.1.0.gem")
+    result = execute("vagrant", "plugin", "install", path.to_s)
+    expect(result).to exit_with(0)
+    expect(result.stdout).to match_output(
+      :plugin_installed, "vagrant-spec-helper-basic", "0.1.0")
+
+    status("Test: plugin shows up in list")
+    result = execute("vagrant", "plugin", "list")
+    expect(result).to exit_with(0)
+    expect(result.stdout).to match_output(
+      :plugin_list_plugin, "vagrant-spec-helper-basic", "0.1.0")
+
+    status("Test: plugin can be uninstalled")
+    result = execute("vagrant", "plugin", "uninstall", "vagrant-spec-helper-basic")
+    expect(result).to exit_with(0)
+
+    status("Test: should no longer have any plugins")
+    result = execute("vagrant", "plugin", "list")
+    expect(result).to exit_with(0)
+    expect(result.stdout).to match_output(:plugin_list_none)
+  end
+
   describe "plugin install" do
     it "fails with a plugin that doesn't exist" do
       name = "vagrant-spec-nope-#{Time.now.to_i}"
@@ -17,19 +41,42 @@ describe "vagrant CLI: plugin", component: "cli/plugin" do
       expect(result.stderr).to match_output(:plugin_install_not_found, name)
     end
 
-    it "succeeds in installing a local plugin" do
-      status("Test: plugin installation")
-      path = Vagrant::Spec.acceptance_plugin_path.join("vagrant-spec-helper-basic-0.1.0.gem")
-      result = execute("vagrant", "plugin", "install", path.to_s)
+    it "succeeds with a remote plugin" do
+      name = "vagrant-spec-helper-basic"
+      result = execute("vagrant", "plugin", "install", name)
       expect(result).to exit_with(0)
       expect(result.stdout).to match_output(
-        :plugin_installed, "vagrant-spec-helper-basic", "0.1.0")
+        :plugin_installed, name, "0.2.0")
+    end
+
+    it "can install a specific version of a plugin" do
+      name = "vagrant-spec-helper-basic"
+      result = execute("vagrant", "plugin", "install", name, "--plugin-version", "0.1.0")
+      expect(result).to exit_with(0)
+      expect(result.stdout).to match_output(
+        :plugin_installed, name, "0.1.0")
+    end
+  end
+
+  describe "plugin update" do
+    it "can update a plugin" do
+      name = "vagrant-spec-helper-basic"
+
+      status("Test: install low version of plugin")
+      result = execute("vagrant", "plugin", "install", name, "--plugin-version", "0.1.0")
+      expect(result).to exit_with(0)
+      expect(result.stdout).to match_output(
+        :plugin_installed, name, "0.1.0")
+
+      status("Test: update the plugin")
+      result = execute("vagrant", "plugin", "update", name)
+      expect(result).to exit_with(0)
 
       status("Test: plugin shows up in list")
       result = execute("vagrant", "plugin", "list")
       expect(result).to exit_with(0)
       expect(result.stdout).to match_output(
-        :plugin_list_plugin, "vagrant-spec-helper-basic", "0.1.0")
+        :plugin_list_plugin, name, "0.2.0")
     end
   end
 end
