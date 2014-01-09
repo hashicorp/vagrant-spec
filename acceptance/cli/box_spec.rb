0b4a10ae45af82b6b1fcbae3a6d7cad25a2b1901
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-01-09 15:55:00 -0800

    acceptance/cli/box: add tests for the --provider flag

--- acceptance/cli/box_spec.rb
+++ acceptance/cli/box_spec.rb
@@ -12,22 +12,52 @@ describe "vagrant CLI: box", component: "cli/box" do
     expect(result.stdout).to match_output(:box_list_no_boxes)
   end
 
-  it "can add a box" do
-    assert_execute("vagrant", "box", "add", "foo", empty_box.to_s)
+  context "basic adding" do
+    it "can add a box" do
+      assert_execute("vagrant", "box", "add", "foo", empty_box.to_s)
 
-    result = execute("vagrant", "box", "list")
-    expect(result).to exit_with(0)
-    expect(result.stdout).to match_output(:box_list_box, "foo")
+      result = execute("vagrant", "box", "list")
+      expect(result).to exit_with(0)
+      expect(result.stdout).to match_output(:box_list_box, "foo")
+    end
+
+    it "can't add a box that exists" do
+      assert_execute("vagrant", "box", "add", "foo", empty_box.to_s)
+      result = execute("vagrant", "box", "add", "foo", empty_box.to_s)
+      expect(result).to exit_with(1)
+    end
+
+    it "can force add a box that exists" do
+      assert_execute("vagrant", "box", "add", "foo", empty_box.to_s)
+      result = execute("vagrant", "box", "add", "--force", "foo", empty_box.to_s)
+      expect(result).to exit_with(0)
+      expect(result.stdout).to match_output(:box_list_added, "foo")
+    end
   end
 
-  it "can't add a box that exists" do
-    assert_execute("vagrant", "box", "add", "foo", empty_box.to_s)
-    result = execute("vagrant", "box", "add", "foo", empty_box.to_s)
-    expect(result).to exit_with(1)
-  end
+  context "adding with a --provider flag" do
+    it "can add with the --provider flag" do
+      assert_execute("vagrant", "box", "add",
+        "foo", empty_box.to_s, "--provider", "empty_provider")
 
-  it "can force add a box that exists" do
-    assert_execute("vagrant", "box", "add", "foo", empty_box.to_s)
-    assert_execute("vagrant", "box", "add", "--force", "foo", empty_box.to_s)
+      result = execute("vagrant", "box", "list")
+      expect(result).to exit_with(0)
+      expect(result.stdout).to match_output(:box_list_box, "foo")
+    end
+
+    it "can force add a box that exists with the --provider flag" do
+      assert_execute("vagrant", "box", "add", "foo", empty_box.to_s)
+      result = execute("vagrant", "box", "add", "--force",
+          "--provider", "empty_provider", "foo", empty_box.to_s)
+      expect(result).to exit_with(0)
+      expect(result.stdout).to match_output(:box_list_added, "foo")
+    end
+
+    it "fails to add a box if the provider given doesn't match" do
+      result = execute("vagrant", "box", "add",
+                       "foo", empty_box.to_s, "--provider", "wrong")
+      expect(result).to exit_with(1)
+      expect(result.stderr).to match_output(:box_add_wrong_provider)
+    end
   end
 end
--- acceptance/output/box_output.rb
+++ acceptance/output/box_output.rb
@@ -2,6 +2,16 @@ require "vagrant-spec/acceptance/output"
 
 module Vagrant
   module Spec
+    # Tests that box add failed with a bad provider
+    OutputTester[:box_add_wrong_provider] = lambda do |text|
+      text =~ /doesn't match the provider/
+    end
+
+    # Tests that box list has a certain box
+    OutputTester[:box_list_added] = lambda do |text, name|
+      text =~ /^Successfully added box '#{name}'/
+    end
+
     # Tests that box list has no boxes.
     OutputTester[:box_list_no_boxes] = lambda do |text|
       text =~ /There are no installed boxes/
