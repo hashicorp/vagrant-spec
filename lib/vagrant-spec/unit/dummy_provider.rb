1df5a3af81cb7cce568b2eac52b8f6822bcb1d8e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-12-10 09:08:49 -0800

    Update dummy provider

--- lib/vagrant-spec/unit/dummy_provider.rb
+++ lib/vagrant-spec/unit/dummy_provider.rb
@@ -14,10 +14,37 @@ module Vagrant
     end
 
     class DummyProvider < Vagrant.plugin("2", :provider)
+      def initialize(machine)
+        @machine = machine
+      end
+
       def action(name)
         lambda do |env|
           # Do nothing
         end
+      end
+
+      def state=(id)
+        state_file.open("w+") do |f|
+          f.write(id.to_s)
+        end
+      end
+
+      def state
+        if !state_file.file?
+          new_state = @machine.id
+          new_state = Vagrant::MachineState::NOT_CREATED_ID if !new_state
+          self.state = new_state
+        end
+
+        state_id = state_file.read.to_sym
+        Vagrant::MachineState.new(state_id, state_id.to_s, state_id.to_s)
+      end
+
+      protected
+
+      def state_file
+        @machine.data_dir.join("dummy_state")
       end
     end
   end
