7bf98cd07ed382b3fe0e39ffb1d65e2bcc4fb496
sophia <scastellarin95@gmail.com>
2020-06-29 14:47:38 -0500

    Separate tests for before and after reboot

--- acceptance/synced_folder/smb_spec.rb
+++ acceptance/synced_folder/smb_spec.rb
@@ -25,5 +25,20 @@ shared_examples "provider/synced_folder/smb" do |provider, options|
     status("Test: doesn't mount a disabled folder")
     result = execute("vagrant", "ssh", "-c", "test -d /foo")
     expect(result.exit_code).to eql(1)
+
+    status("Test: persists a sync folder after a manual reboot")
+    result = execute("vagrant", "ssh", "-c", "sudo reboot")
+    expect(result).to exit_with(255)
+    result = execute("vagrant", "ssh", "-c", "cat /vagrant-smb/foo")
+    expect(result.exit_code).to eql(0)
+    expect(result.stdout).to match(/hello$/)
+
+    status("Test: persists a sync folder after a provisioner reboot")
+    result = execute("vagrant", "provision", "--provision-with", "reboot")
+    expect(result.exit_code).to eql(0)
+    sleep 10
+    result = execute("vagrant", "ssh", "-c", "cat /vagrant-smb/foo")
+    expect(result.exit_code).to eql(0)
+    expect(result.stdout).to match(/hello$/)
   end
 end
