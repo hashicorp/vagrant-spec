461e917e78d0f256827a953d5d49bc67e030df4c
sophia <scastellarin95@gmail.com>
2020-09-17 14:44:55 -0500

    Skip SMB tests for linux hosts
    SMB is not supported for linux hosts
    
    ref: https://www.vagrantup.com/docs/synced-folders/smb
    "SMB is currently only supported when the host machine is Windows or macOS. The guest machine can be Windows, Linux, or macOS."

--- lib/vagrant-spec/acceptance/rspec.rb
+++ lib/vagrant-spec/acceptance/rspec.rb
@@ -10,4 +10,8 @@ RSpec.configure do |c|
   if ENV["VAGRANT_SPEC_GUEST_PLATFORM"].to_s == "windows"
     c.filter_run_excluding :skip_windows_guest
   end
+
+  if RUBY_PLATFORM.match(/linux/)
+    c.filter_run_excluding :skip_linux_hosts
+  end
 end
